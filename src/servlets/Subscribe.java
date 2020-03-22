7555a52ab8397b4b1c6d99ec92a73e73ece58249
KevinBal <balavoine.kevin@gmail.com>
2020-03-22 15:13:17 +0100

    Reglage petite chose master

--- src/servlets/Subscribe.java
+++ src/servlets/Subscribe.java
@@ -2,8 +2,6 @@ package servlets;
 
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
